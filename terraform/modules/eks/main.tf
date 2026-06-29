86f51c938f7c6c7dc8e297b46fd02daeec3e292d
Azamcloudspace <azamiheme19@gmail.com>
2026-06-29 17:01:12 +0100

    edit workflows and and eks module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -53,4 +53,21 @@ resource "aws_eks_access_policy_association" "github_actions" {
   access_scope {
     type = "cluster"
   }
+
 }
+
+resource "aws_eks_access_entry" "local_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::760773573647:user/azamcloudspace"
+}
+
+resource "aws_eks_access_policy_association" "local_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::760773573647:user/azamcloudspace"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
